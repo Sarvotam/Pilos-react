56bc4c0923e6a518425368a0c332dda382db7380
Sarvottam Hengaju <hengajusarvottam@gmail.com>
2024-01-02 11:08:28 +0545

    fetch user data from form and error handeling

--- src/components/pages/auth/UserLogin.js
+++ src/components/pages/auth/UserLogin.js
@@ -1,37 +1,49 @@
-import {TextField, Button, Box, Alert} from '@mui/material'
-import { NavLink } from 'react-router-dom'
+import { TextField, Button, Box, Alert } from '@mui/material'
+import { useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 // import React from 'react'
 
 const UserLogin = () => {
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		// FormData Default from web api [ Could be solved through STATE as well]
-		const data = new FormData(e.currentTarget);
-		const actualData ={
-			email: data.get('email'),
-			password: data.get('password'),
+	const [error, setError] = useState({
+		status: false,
+		msg: "",
+		type: ""
+	})
+
+	const navigate = useNavigate();
+		const handleSubmit = (e) => {
+			e.preventDefault();
+			// FormData Default from web api [ Could be solved through STATE as well]
+			const data = new FormData(e.currentTarget);
+			const actualData = {
+				email: data.get('email'),
+				password: data.get('password'),
+			}
+			
+			// work for validation
+			if (actualData.email && actualData.password) {
+				// console.log(actualData);
+				// clear fields after submission complete
+				document.getElementById('login-form').reset()
+				setError({status: true, msg:"Login Success ", type: 'success'})
+				navigate('/')
+			} else {
+				setError({ status: true, msg: "All fields are required", type: 'error' })
+			}
 		}
-		// work for validation
-		if(actualData.email && actualData.password){
-		console.log(actualData);
-		// clear fields after submission complete
-		document.getElementById('login-form').reset()
-		}else{
-		console.log("All fields are required");
-		}
-	}
-  return (
-    <>
-      <Box component='form' noValidate sx={{ mt: 1}} id='login-form' onSubmit={handleSubmit}>
-        <TextField margin='normal' required fullWidth id='email' name='email' label='Email Address' />
-        <TextField margin='normal' required fullWidth id='password' name='password' label='password' type='password' />
-        <Box textAlign='center'>
-					<Button type='submit' variant='contained' sx={{ mt:3, mb: 2, px: 5}} >Login</Button>
-        </Box>
-				<NavLink to='/' >Forgot Password?</NavLink>
-      </Box>
-    </>
-  )
+		return (
+			<>
+				<Box component='form' noValidate sx={{ mt: 1 }} id='login-form' onSubmit={handleSubmit}>
+					<TextField margin='normal' required fullWidth id='email' name='email' label='Email Address' />
+					<TextField margin='normal' required fullWidth id='password' name='password' label='password' type='password' />
+					<Box textAlign='center'>
+						<Button type='submit' variant='contained' sx={{ mt: 3, mb: 2, px: 5 }} >Login</Button>
+					</Box>
+					<NavLink to='/' >Forgot Password?</NavLink>
+					{error.status ? <Alert severity={error.type}>{error.msg}</Alert> : ''}
+				</Box>
+			</>
+		)
 }
 export default UserLogin
--- src/components/pages/auth/UserRegister.js
+++ src/components/pages/auth/UserRegister.js
@@ -1,24 +1,57 @@
-import {TextField, Button, Box, Alert} from '@mui/material'
-import { NavLink } from 'react-router-dom'
-
-// import React from 'react'
+import {TextField, FormControlLabel, Checkbox, Button, Box, Alert} from '@mui/material'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const UserRegister = () => {
-    return (
-        <>
-          <Box component='form' noValidate sx={{ mt: 1}} id='login-form'>
-            <TextField margin='normal' required fullWidth id='email' name='email' label='Email Address' />
-            <TextField margin='normal' required fullWidth id='UserName' name='User Name' label='User Name' />
+	const [error, setError] = useState({
+		status: false,
+		msg: "",
+		type: ""
+	})
 
-            <TextField margin='normal' required fullWidth id='password' name='password' label='password' type='password' />
-            <TextField margin='normal' required fullWidth id='password confirmation ' name='password confirmation' label='password confirmation' type='password confirmation' />
-
-            <Box textAlign='center'>
-                <Button type='submit' variant='contained' sx={{ mt:3, mb: 2, px: 5}} >Register</Button>
-            </Box>
-          </Box>
-        </>
-      )
+	const navigate = useNavigate();
+		const handleSubmit = (e) => {
+			e.preventDefault();
+			// FormData Default from web api [ Could be solved through STATE as well]
+			const data = new FormData(e.currentTarget);
+			const actualData = {
+				email: data.get('email'),
+        name: data.get('name'),
+				password: data.get('password'),
+				password_confirmation: data.get('password_confirmation'),
+        tc: data.get('tc'),
+			}
+			
+			// work for validation
+			if (actualData.email && actualData.password && actualData.name && actualData.tc !== null) {
+				if(actualData.password === actualData.password_confirmation){
+          console.log(actualData);
+				// clear fields after submission complete
+				document.getElementById('registration-form').reset()
+				setError({status: true, msg:"Registration Success ", type: 'success'})
+				navigate('/')
+        }else{
+          setError({status: true, msg: "Password and password confirmation doesnot match", type: 'error'})
+        }
+			} else {
+				setError({ status: true, msg: "All fields are required", type: 'error' })
+			}
+		}
+		return (
+			<>
+				<Box component='form' noValidate sx={{ mt: 1 }} id='registration-form' onSubmit={handleSubmit}>
+					<TextField margin='normal' required fullWidth id='email' name='email' label='Email Address' />
+					<TextField margin='normal' required fullWidth id='name' name='name' label='Name' />
+					<TextField margin='normal' required fullWidth id='password' name='password' label='password' type='password' />
+					<TextField margin='normal' required fullWidth id='password_confirmation' name='password_confirmation' label='Confirm Password' type='password' />
+          <FormControlLabel control={<Checkbox value="agree" color="primary" name='tc' id='tc' />} label="I agree to terms and condition." />
+					<Box textAlign='center'>
+						<Button type='submit' variant='contained' sx={{ mt: 3, mb: 2, px: 5 }} >Join</Button>
+					</Box>
+					{error.status ? <Alert severity={error.type}>{error.msg}</Alert> : ''}
+				</Box>
+			</>
+		)
 }
 
 export default UserRegister
